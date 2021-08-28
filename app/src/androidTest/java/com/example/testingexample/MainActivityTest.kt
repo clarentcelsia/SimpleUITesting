43a21476ed9e64cc900d288bfa404796a33fb001
clarenti <clarentic@gmail.com>
2021-08-28 19:06:53 +0700

    Gallery Intents

--- app/src/androidTest/java/com/example/testingexample/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/testingexample/MainActivityTest.kt
@@ -1,12 +1,29 @@
 package com.example.testingexample
 
-import androidx.test.core.app.ActivityScenario
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.ContentResolver
+import android.content.Intent
+import android.content.res.Resources
+import android.net.Uri
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasType
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.platform.app.InstrumentationRegistry
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -18,14 +35,69 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4ClassRunner::class)
 class MainActivityTest {
 
-    // This checks whether the activity works or not
-    // Activity should appear on screen matched by the given matcher
-    // The given matcher is the given id.
-    @Test
-    fun test_isActivityInView() {
-        // ActivityScenario is part of AndroidX library
-        // That can place the activity to different states
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-        onView(withId(R.id.main_root)).check(matches(isDisplayed()))
+    // Initialize espresso-intents rule
+    val intent = Intent(ApplicationProvider.getApplicationContext(), MainActivity::class.java)
+
+    @get: Rule
+    val activityRule = ActivityScenarioRule<MainActivity>(intent)
+
+    @Before
+    fun init(){
+        Intents.init()
     }
+
+    @Test
+    fun test_validateIntent() {
+
+        val expectedIntent: Matcher<Intent> = allOf(
+            //Pass all the things that did in Intent to MainActivity
+            hasAction(Intent.ACTION_GET_CONTENT),
+            hasType("image/*")
+        )
+
+        val activityForResult = createGalleryStub()
+
+        //Testing intent
+        intending(expectedIntent).respondWith(activityForResult)
+
+        //Verify the action used to launch the activity
+        //produces the stub result.
+        onView(withId(R.id.open_gallery)).perform(click())
+        intended(expectedIntent)
+    }
+
+    @After
+    fun release(){
+        Intents.release()
+    }
+
+    /**
+     *  Stubbing helps to deal with activityForResult
+     *  to make sure the intent provide the data callback response
+     *  after doing the intent.
+     *
+     *  This is useful to check whether the intent response works as expected
+     *  or not since it can't be done under test to manipulate the ui
+     *  of external activity or control the ActivityResult returned to Activity.
+     */
+
+    private fun createGalleryStub(): Instrumentation.ActivityResult {
+        // Test pick image from drawable
+        val resource: Resources = InstrumentationRegistry.getInstrumentation()
+            .context.resources
+
+        // Get the uri of an image stored in drawable
+        val imageUri = Uri.parse(
+            ContentResolver.SCHEME_ANDROID_RESOURCE + "://" +
+                    resource.getResourcePackageName(R.drawable.ic_launcher_background) + "/" +
+                    resource.getResourceTypeName(R.drawable.ic_launcher_background) + "/" +
+                    resource.getResourceEntryName(R.drawable.ic_launcher_background)
+        )
+
+        val resultData = Intent()
+        resultData.data = imageUri
+
+        return Instrumentation.ActivityResult(Activity.RESULT_OK, resultData)
+    }
+
 }
--- app/src/main/java/com/example/testingexample/MainActivity.kt
+++ app/src/main/java/com/example/testingexample/MainActivity.kt
@@ -1,32 +1,46 @@
 package com.example.testingexample
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Parcelable
+import android.view.View.VISIBLE
 import android.widget.Button
-import com.example.testingexample.factory.AppFragmentFactory
-import com.example.testingexample.model.Books
-import com.example.testingexample.model.Data
-import com.example.testingexample.source.RemoteDataSource
-import java.util.ArrayList
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var container: ImageView
+    private lateinit var btn_open: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        supportFragmentManager.fragmentFactory = AppFragmentFactory()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if(supportFragmentManager.fragments.size == 0){
-            val data = 1
-            val bundle = Bundle()
-            bundle.putInt("bookID", data)
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment::class.java, bundle)
-                .commit()
-        }
+        container = findViewById(R.id.container)
+        btn_open = findViewById(R.id.open_gallery)
 
+        btn_open.setOnClickListener {
+            open_gallery()
+        }
+    }
+
+    private fun open_gallery(){
+        val intent = Intent()
+            .setType("image/*")
+            .setAction(Intent.ACTION_GET_CONTENT)
+
+        resultLauncher.launch(intent)
+    }
+
+    private val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if (it.resultCode == Activity.RESULT_OK){
+            val data = it.data?.data
+            container.visibility = VISIBLE
+            Glide.with(this).load(data).into(container)
+        }
     }
 }
 
